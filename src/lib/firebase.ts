cb235bb58a866218cd25874848302d7c4351bb3b
nonokaM <c1201071@gmail.com>
2023-10-27 15:58:34 +0900

    add: firebase setting

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUKKFzmXA97fSOuqjtI7o_vyNXR65tr7M",
+  authDomain: "pd-quiz-59560.firebaseapp.com",
+  projectId: "pd-quiz-59560",
+  storageBucket: "pd-quiz-59560.appspot.com",
+  messagingSenderId: "459107770615",
+  appId: "1:459107770615:web:aaedd08285494038b573f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
