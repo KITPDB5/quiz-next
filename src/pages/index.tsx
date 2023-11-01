0aad3055d0ecd5edf8741fe819de36e56143e978
nonokaM <c1201071@gmail.com>
2023-11-01 14:46:37 +0900

    add: ページ遷移ボタンを実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <p>Home</p>
+      <Link href="/quiz">
+        クイズを作成する
+      </Link>
     </div>
   )
 }
