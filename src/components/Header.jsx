e04540bff96001a032b962a679a6439acd9fb9c1
NonokaM <c1201071@gmail.com>
2024-01-19 15:40:39 +0900

    refactor: Remove Header component and import from _app.tsx

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Header() {
-  return (
-    <header>
-      <Link href="/">{/* header(ホームに戻る) */}</Link>
-    </header>
-  );
-}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import '@/styles/globals.css'
-import Header from '@/components/Header'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
       <Component {...pageProps} />
     </>
   )
